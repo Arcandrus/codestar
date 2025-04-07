58803643e0f287afea8d678707ed7862bbd9b235
Arcandrus <eric.harper87@gmail.com>
2025-04-07 14:58:38 +0100

    Added missing . to allowed host

--- codestar/settings.py
+++ codestar/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure--94durb2w3mrnn@3n=nbrb2*m%0b7#6r9v9&uw&=okp4hy^#c!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['herokuapp.com',
+ALLOWED_HOSTS = ['.herokuapp.com',
                  '127.0.0.1',]
 
 
